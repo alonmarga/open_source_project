44fdd5e3598f8790591321e66de88611dcb5aa11
Alon Margalit <alonmarga@gmail.com>
2024-12-24 15:28:58 +0200

    added test handler

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,2 +1,10 @@
-def start(update, context):
-    update.message.reply_text("Hello! I'm your Telegram bot!")
+# def start(update, context):
+#     update.message.reply_text("Hello! I'm your Telegram bot!")
+
+
+async def start(update, context):
+    await update.message.reply_text("Hello! I'm your Telegram bot!")
+
+
+async def test(update, context):
+    await update.message.reply_text("test! I'm your test Telegram bot!")
--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,11 @@
 from dotenv import load_dotenv
-from handlers.start import start  # Import handlers
+from handlers.start import start, test  # Import handlers
 import os
 from telegram.ext import Application, CommandHandler, Updater
 
 load_dotenv()
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-
-async def start(update, context):
-    await update.message.reply_text("Hello! I'm your Telegram bot!")
 
 # Main function
 def main():
@@ -18,7 +15,7 @@ def main():
 
     # Register handlers
     application.add_handler(CommandHandler("start", start))
-
+    application.add_handler(CommandHandler("test",test))
     # Start the bot
     application.run_polling()
 
