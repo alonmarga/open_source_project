6a57a25269ee85fa53df66d329eba2e11dba46db
Alon Margalit <alonmarga@gmail.com>
2025-01-25 14:58:12 +0200

    spark reads file from minio

--- spark_scripts/read.py
+++ spark_scripts/read.py
@@ -1,22 +1,21 @@
 from pyspark.sql import SparkSession
 
 def main():
-    print("hi")
-
     spark = None
     try:
-        spark = SparkSession.builder \
+        spark = (SparkSession.builder \
             .appName("PySpark Example") \
+            .master("spark://spark-master:7077") \
             .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
             .config("spark.hadoop.fs.s3a.access.key", "K02e2gI7iOKwQrV6RP8j") \
             .config("spark.hadoop.fs.s3a.secret.key", "dn2Gi8jKA9y4QLZdYiSzVXcPCNn7TFis6bhzwATi") \
             .config("spark.hadoop.fs.s3a.path.style.access", "true") \
             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-            .getOrCreate()
+            .getOrCreate())
 
-        print("sparksession", spark)
         df = spark.read.csv("s3a://my-json-storage/orders_20250109_000435.json", header=True)
         df.show()
+
 
     except Exception as e:
         print(f"Error while reading the CSV file: {e}")
